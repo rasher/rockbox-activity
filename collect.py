c80c13fde461acb5101873eb2c265af8b53959a7
Jonas Häggqvist <rasher@rasher.dk>
2010-09-14 00:19:12 +0200

    Add collect script which gathers everything. A bit messy atm

---
+++ collect.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# vim: set fileencoding=UTF-8 :
+#
+# Copyright (c) 2010, Jonas Häggqvist <rasher@rasher.dk>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * Neither the name of the program nor the names of its contributors may be
+#   used to endorse or promote products derived from this software without
+#   specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+import forum
+import irc
+import svn
+import ml
+import re
+
+def config(v):
+    c = dict([x.split(':') for x in open('config').readlines()])
+    return c[v].strip()
+
+if __name__ == "__main__":
+    users = {
+            'Jonas Häggqvist': {
+                'ml': [u'Jonas Häggqvist', 'Jonas H'],
+                'irc': ['rasher', 'rasher_', re.compile('(?i)rasher.*'), re.compile('(?i)bacon_.*')],
+                'svn': ['rasher'],
+                'forum': ['rasher'],
+                    },
+                }
+    mldir = 'ml'
+    irclogs = 'irclogs'
+    ml.update(mldir)
+    irc.update(irclogs)
+    forum.login(config('forumuser'), config('forumpass'))
+    
+    activity = {
+            'ml': ml.getactivity(mldir, users),
+            'IRC' : irc.getactivity(irclogs, users),
+            'SVN' : svn.getactivity(users),
+            'Forum': forum.getactivity(users),
+    }
+    units = {
+            'ml':'mail',
+            'IRC':'line',
+            'SVN':'commit',
+            'Forum':'post',
+            }
+    for user in users:
+        print user
+        for loc in activity:
+            if user in activity[loc]:
+                count = len(activity[loc][user])
+                unit = units[loc]
+                s = "s"
+                if unit == 1:
+                    s = ""
+                print "  %s%s %s%s" % (loc.ljust(7, '.'), str(count).rjust(7, '.'), unit, s)
